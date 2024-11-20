276ad07c5d5d85e944f50d48093fbaf3d4ba7d9f
lakshan <lakshanganesan05@gmail.com>
2024-11-20 19:40:07 +0530

    Your commit message here

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CamPageComponent } from './cam-page/cam-page.component';
-import { ImagePageComponent } from './image-page/image-page.component';
 
 export const routes: Routes = [
   {
@@ -12,8 +11,5 @@ export const routes: Routes = [
     path: 'webcam',
     component: CamPageComponent,
   },
-  {
-    path: 'image',
-    component: ImagePageComponent,
-  },
+ 
 ];
